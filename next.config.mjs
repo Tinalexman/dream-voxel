d322c3c3da326b4de47e5f2f20a990374bfe4adc
Tinalexman <taiwoluwatobilobafestus@gmail.com>
2024-04-01 19:46:38 +0100

    Setting UP

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+          config.module.rules.push({
+            test: /\.(glsl|vs|fs|vert|frag)$/,
+            exclude: /node_modules/,
+            use: ['raw-loader', 'glslify-loader'],
+          });
+        }
+        return config;
+      },
+};
 
 export default nextConfig;
